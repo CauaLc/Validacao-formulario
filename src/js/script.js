76367fce9f712dad6ad5b7fe47e4a31049282c82
Cauã Luz <125490757+CauaLc@users.noreply.github.com>
2024-02-13 11:31:07 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+const inputsVazios = document.querySelectorAll('.input');
+
+const formulario = document.querySelector('.formulario');
+
+
+inputsVazios.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value !== "") {
+            input.classList.add('preenchido')
+        }
+    })
+})
+
+
+formulario.addEventListener('submit', function(event) { 
+    inputsVazios.forEach(input => {
+        if (input.value === "") {
+            event.preventDefault();
+            input.classList.add('mudar-cor');
+            input.nextElementSibling.classList.add('campo-invalido');
+        } else {
+            return true
+        }
+    })
+})
+
